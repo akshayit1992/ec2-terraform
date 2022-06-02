a01c25d00e569edf5296c7c8a146157785791bd9
Akshay Shelke <101508942+akshayit1992@users.noreply.github.com>
2022-06-02 23:23:53 +0530

    newbucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-buckets3-forterraformtest"
+    bucket         = "newjenkins-buckets3-forterraformtest"
     key            = "tf-statefile-bucket"
     region         = "ap-south-1"
     dynamodb_table = "dymodb-lock"
